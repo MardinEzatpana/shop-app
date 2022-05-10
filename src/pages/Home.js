321747fe45bfa4fd45bc4a77e0130c76db00c461
MardinEzatpana <madin.ezatpana@gmail.com>
2022-05-10 13:48:46 +0430

    push local project to my repos github

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="container mt-5">
       <div className="row text-center">
         <div className="col-md-12">
-          <h3>Shopping Store - Pelazio</h3>
+          <h1>Shopping Store - Pelazio</h1>
           <p className="mt-4">
             Pelazio Company is an online store with a kind of products from a
             variety of goods that you can choose the best products in the best
