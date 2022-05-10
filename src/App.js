dbae18063b7cd07214e21bafc8303e1f615cc9a7
MardinEzatpana <madin.ezatpana@gmail.com>
2022-05-10 16:20:04 +0430

    create header and add to app route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter , Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 const App = () => {
   return <>
    <BrowserRouter>
-   
+   <Header />
    <Routes>
      <Route exact path="/" element={<Home/>} />
    </Routes>
